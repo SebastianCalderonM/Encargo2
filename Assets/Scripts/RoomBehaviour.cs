38a219288eba00f8aba8554536f7338852f0ddc6
SebastianCalderonM <Sebastian.Calderon@externos.afpcapital.cl>
2023-10-13 21:19:40 -0300

    Dungeon Generator

--- Assets/Scripts/RoomBehaviour.cs
+++ Assets/Scripts/RoomBehaviour.cs
@@ -7,16 +7,9 @@ public class RoomBehaviour : MonoBehaviour
     public GameObject[] walls; // Orden elementos 0 - North 1 - South 2 - East 3 - West
     public GameObject[] doors;
     
-    public bool[] test;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        UpdateRoom(test);
-    }
 
     // Update is called once per frame
-    void UpdateRoom(bool[] status)
+    public void UpdateRoom(bool[] status)
     {
         for (int i=0; i < status.Length; i++)
         {
