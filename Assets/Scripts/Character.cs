e4cabddc949fd097603a1b39bd15d11c3aab9262
SebastianCalderonM <Sebastian.Calderon@externos.afpcapital.cl>
2023-10-15 17:18:49 -0300

    Character View Direction

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -6,7 +6,10 @@ public class Character : MonoBehaviour
 {
     private CharacterController characterController;
    
-   public float Speed = 5f;
+    private Vector3 _direction;
+    [SerializeField] private float smoothTime = 0.05f;
+    private float _currentVelocity;
+    public float Speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,15 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if ((new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"))).sqrMagnitude == 0) return;
+
+        
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        
+        var targetAngle = Mathf.Atan2(move.x, move.z)* Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+        
         characterController.Move(move*Time.deltaTime*Speed);
     }
 }
