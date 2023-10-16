4ea7f0bacd0883e1a771569e80fddc6c26401a06
NandyUrie <nandy.troncoso@usm.cl>
2023-10-15 22:33:18 -0300

    enemigos

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -8,15 +8,20 @@ public class DungeonGenerator : MonoBehaviour
     {
         public bool visited = false;
         public bool[] status = new bool[4];
+        public Vector3 centroHabitacion;
     }
 
     public Vector2 size;
     public int startPos = 0;
     public GameObject room;
     public Vector2 offset;
+    
+    public GameObject cubo;
+    public GameObject cilindro;
+    public GameObject esfera;
+    public GameObject final;
 
     List<Cell> board;
-
     
     // Start is called before the first frame update
     void Start()
@@ -30,25 +35,7 @@ public class DungeonGenerator : MonoBehaviour
         
     }
 
-    void GenerateDungeon()
-    {
-        for (int i = 0; i < size.x; i++)
-        {
-            for (int j = 0; j < size.y; j++)
-            {
-                Cell currentCell = board[Mathf.FloorToInt(i+j*size.x)];
-                if (currentCell.visited)
-                { 
-                    var newRoom = Instantiate(room, new Vector3(i*offset.x,0,-j*offset.y), Quaternion.identity, transform).GetComponent<RoomBehaviour>();
-                    newRoom.UpdateRoom(board[Mathf.FloorToInt(i+j*size.x)].status);
-
-                    newRoom.name += " "+i+"-"+j;
-                }
-            }
-        }
-    }
-
-    void MazeGenerator()
+    public void MazeGenerator()
     {
         board = new List<Cell>();
 
@@ -135,6 +122,40 @@ public class DungeonGenerator : MonoBehaviour
         GenerateDungeon();
     }
 
+    public void GenerateDungeon()
+    {
+        for (int i = 0; i < size.x; i++)
+        {
+            for (int j = 0; j < size.y; j++)
+            {
+                Cell currentCell = board[Mathf.FloorToInt(i+j*size.x)];
+                if (currentCell.visited)
+                { 
+                    currentCell.centroHabitacion = new Vector3( i * offset.x , 0 , -j * offset.y );
+                    var newRoom = Instantiate(room, currentCell.centroHabitacion, Quaternion.identity, transform).GetComponent<RoomBehaviour>();
+                    newRoom.UpdateRoom(board[Mathf.FloorToInt(i+j*size.x)].status);
+
+                    newRoom.name += " "+i+"-"+j;
+
+                    if( (i != 0 && j != 0) && (currentCell != board[board.Count-1]))
+                    {
+                        int cantidadEnemigosHab = Random.Range(0,4);
+
+                        for (int n = 0 ; n < cantidadEnemigosHab ; n++)
+                        {
+                            SeleccionarEnemigo(currentCell);
+                        }
+                    }
+                    
+                    
+                }
+            }
+        }
+        Vector3 paja = new Vector3(board[board.Count-1].centroHabitacion.x, 0.011f, board[board.Count-1].centroHabitacion.z);
+
+        final = Instantiate(final, paja , Quaternion.identity, transform);
+    }
+
     List<int> CheckNeighbors(int cell)
     {
         List<int> neighbors = new List<int>();
@@ -165,5 +186,26 @@ public class DungeonGenerator : MonoBehaviour
 
 
         return neighbors;
+    }
+
+    public void SeleccionarEnemigo(DungeonGenerator.Cell celda)
+    {
+        GameObject enemy;
+        int aux = Random.Range(0,3);
+        Vector3 posRand = new Vector3( Random.Range(celda.centroHabitacion.x  - 5 , celda.centroHabitacion.x + 5) , 0.5f , Random.Range(celda.centroHabitacion.z  - 5 , celda.centroHabitacion.z + 5));
+
+        if (aux == 0 )
+        {
+            enemy = Instantiate(cubo, posRand, Quaternion.identity, transform);
+        }
+        else if (aux == 1)
+        {
+            enemy = Instantiate(esfera, posRand, Quaternion.identity, transform);
+        }
+        else
+        {
+            enemy = Instantiate(cilindro, posRand, Quaternion.identity, transform);
+        }
+
     }
 }
